943cd2dc00795d71f56a7785b3491b84723ee12e
s7thiago <thyagosousasilva@gmail.com>
2019-11-29 08:07:21 -0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:my_flutter_showcase/screens/parallax_example/HomeSimpleParallax.
 import 'package:my_flutter_showcase/screens/plant_app_design/plant_app_design.dart';
 import 'package:my_flutter_showcase/screens/sign_up_design/SugnUpDesignMain.dart';
 
+import 'screens/MainComponents.dart' as mainComponents;
 import 'screens/firewatch_parallax/firewatch_parallax_main.dart';
 
 main(List<String> args) {
@@ -20,7 +21,7 @@ main(List<String> args) {
 }
 
 // ! The lines above are just a test
-class MainApp extends StatefulWidget {
+class MainApp extends StatefulWidget with mainComponents.MainComponents{
   @override
   _MainAppState createState() => _MainAppState();
 }
@@ -93,54 +94,4 @@ class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
     );
   }
 }
-
 // ! The lines up are just a test
-
-class MyApp extends StatelessWidget with MainComponents {
-  @override
-  Widget build(BuildContext context) {
-    return SamplesList(
-      itens: <Widget>[
-        ScreenItem(
-            representationIcon: Icons.bubble_chart,
-            title: "Food App Design",
-            description: "Just a design",
-            destination: MyFoodApp()),
-        ScreenItem(
-            representationIcon: Icons.nature,
-            /*Ou o icone vintage */
-            title: "Plant App Design",
-            description: "Just a design",
-            destination: PlantAppDesignHome()),
-        ScreenItem(
-            representationIcon: Icons.flip,
-            title: "Simple Parallax",
-            description: "Just a design",
-            destination: HomeSimpleParallax()),
-        ScreenItem(
-            representationIcon: Icons.crop_landscape,
-            title: "Cards App Design",
-            description: "Just a design",
-            destination: CardsAppDesignMain()),
-        ScreenItem(
-          representationIcon: Icons.ac_unit,
-          title: "Animation Samples",
-          description: "Samples of animations on Flutter",
-          destination: SimpleAnimationExample(),
-        ),
-        ScreenItem(
-          representationIcon: Icons.person,
-          title: "Sign up design",
-          description: "Sign up form smartphone design",
-          destination: SignUpDesignMain(),
-        ),
-        ScreenItem(
-          representationIcon: Icons.person,
-          title: "Firewatch Parallax Design",
-          description: "A example of parallax effect in flutter",
-          destination: FirewatchParallaxMain(),
-        ),
-      ],
-    );
-  }
-}
