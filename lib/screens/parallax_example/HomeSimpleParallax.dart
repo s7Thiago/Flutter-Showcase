14894071f84089cf96dcf30d107c184f13397d97
s7thiago <thyagosousasilva@gmail.com>
2019-11-29 08:45:24 -0300

    Correcting screen orientation manager when the example of simple parallax is opened, or closed

--- lib/screens/parallax_example/HomeSimpleParallax.dart
+++ lib/screens/parallax_example/HomeSimpleParallax.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+import '../firewatch_parallax/firewatch_parallax_main.dart'
+    as fireWatchParallaxUil;
 
 class HomeSimpleParallax extends StatelessWidget {
   @override
@@ -25,82 +27,79 @@ class _HomePageSimpleParallaxExampleState
   double topTree = 200;
   double topFor = 300;
 
-  Future<bool> _setupOrientation() {
-    return Future<bool>.value(true).then((_) {
-      setState(() {
-        SystemChrome.setPreferredOrientations(
-                [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp])
-            .then((_) {
-          print("\n\n\nsetupOrientation() foi chamado!");
-        });
-      });
-    });
+  @override
+  void initState() {
+    super.initState();
+    // fireWatchParallaxUil.FirewatchParallaxHome().takeLandscapeModeOnly();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    fireWatchParallaxUil.FirewatchParallaxHome().takePortraitModeOnly();
   }
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () => _setupOrientation(),
-      child: Scaffold(
-        body: NotificationListener(
-          // ignore: missing_return
-          onNotification: (v) {
-            if (v is ScrollUpdateNotification) {
+    return Scaffold(
+      body: NotificationListener(
+        // ignore: missing_return
+        onNotification: (v) {
+          if (v is ScrollUpdateNotification) {
 //            only scrool update notification is triggered
-              setState(() {
-                topOne = topOne - v.scrollDelta / 3;
-                topTwo = topTwo - v.scrollDelta / 1;
-                topTree = topTree - v.scrollDelta / 0.5;
-                topFor = topFor - v.scrollDelta / 0.25;
-              });
-            }
-          },
-          child: Stack(
-            alignment: Alignment.center,
-            children: <Widget>[
-              Positioned(
-                top: topOne,
-                left: 0,
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.red,
-                ),
+            setState(() {
+              topOne = topOne - v.scrollDelta / 3;
+              topTwo = topTwo - v.scrollDelta / 1;
+              topTree = topTree - v.scrollDelta / 0.5;
+              topFor = topFor - v.scrollDelta / 0.25;
+            });
+          }
+        },
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: topOne,
+              left: 0,
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
               ),
-              Positioned(
-                  top: topTwo,
-                  left: 100,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  )),
-              Positioned(
-                  top: topTree,
-                  left: 200,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  )),
-              Positioned(
-                  top: topFor,
-                  left: 300,
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  )),
-              ListView(
-                children: <Widget>[
-                  Container(
-                    height: 600,
-                    color: Colors.transparent,
-                  )
-                ],
-              )
-            ],
-          ),
+            ),
+            Positioned(
+                top: topTwo,
+                left: 100,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                )),
+            Positioned(
+                top: topTree,
+                left: 200,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                )),
+            Positioned(
+                top: topFor,
+                left: 300,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                )),
+            ListView(
+              children: <Widget>[
+                Container(
+                  height: 600,
+                  color: Colors.transparent,
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
