94684fae91ed81d107826f8799df32d615e76f08
s7thiago <thyagosousasilva@gmail.com>
2020-01-15 15:15:25 -0300

    Correcting the issue when the page scroon control with bottom navigation view itens or action buttons in app bar not working in first or last pages

--- lib/screens/animation_example/sample_3/pageview_and_bottom_navigation_view_animation_example.dart
+++ lib/screens/animation_example/sample_3/pageview_and_bottom_navigation_view_animation_example.dart
@@ -5,132 +5,124 @@ class PageViewAndBottomNavigationViewAnimationExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: PageViewAndBottomNavigationViewAnimationExampleHome(),
+      theme: ThemeData.light(),
+      home: PagesAppAnimation(),
     );
   }
 }
 
-class PageViewAndBottomNavigationViewAnimationExampleHome
-    extends StatefulWidget {
+class PagesAppAnimation extends StatefulWidget {
   @override
-  _PageViewAndBottomNavigationViewAnimationExampleHomeState createState() =>
-      _PageViewAndBottomNavigationViewAnimationExampleHomeState();
+  _PagesAppAnimationState createState() => _PagesAppAnimationState();
 }
 
-class _PageViewAndBottomNavigationViewAnimationExampleHomeState
-    extends State<PageViewAndBottomNavigationViewAnimationExampleHome> {
-  var pages = [
+class _PagesAppAnimationState extends State<PagesAppAnimation> {
+  PageController pageController;
+  List<String> pageTitles = [
     'Home',
+    'Feed',
     'Favorites',
     'Account',
+    'Settings'
   ];
-  PageController pageController;
-  Color appBarColor = Colors.red;
-  Color bodyColor = Color(0xffEF6F6C);
-  Color bottomNavigationBarColor = Colors.red;
-  Color pageTextColor = Colors.white;
-  Color appBarTitleColor = Colors.white;
-  Color iconsColor = Color(0xff700548);
-  Color iconsTextColor = Colors.black;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     pageController = PageController(initialPage: 0);
+
+    // Capturing the double that represents the page scrool transition value
+    pageController.addListener(() {
+      print('pageControllerListener: ${pageController.page}');
+    });
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-//    Adicionando um listener para pegar o valor double para a atual pagina
-  pageController.addListener(() {
-    print('pageListener: ${pageController.page.round()}');
-  });
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'PageView',
-          style: TextStyle(color: appBarTitleColor),
+          'PageView Animation',
         ),
-        backgroundColor: appBarColor,
         actions: <Widget>[
           IconButton(
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: iconsColor,
-              ),
               onPressed: () {
                 pageController.previousPage(
-                    duration: Duration(milliseconds: 500),
+                    duration: Duration(
+                      milliseconds: 500,
+                    ),
                     curve: Curves.easeInOutBack);
-              }),
+              },
+              icon: Icon(Icons.arrow_back_ios)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.home)),
           IconButton(
-            focusColor: Colors.black,
-            icon: Icon(Icons.home),
-            color: iconsColor,
-            onPressed: () {
-            pageController.jumpToPage(0);
-            },
-          ),
-          IconButton(
-              icon: Icon(Icons.arrow_forward_ios, color: iconsColor),
               onPressed: () {
                 pageController.nextPage(
-                    duration: Duration(milliseconds: 500),
+                    duration: Duration(
+                      milliseconds: 500,
+                    ),
                     curve: Curves.easeInOutBack);
-              }),
+              },
+              icon: Icon(Icons.arrow_forward_ios)),
         ],
       ),
-      body: Container(
-        color: bodyColor,
-        child: PageView.builder(
-            controller: pageController,
-            physics: ClampingScrollPhysics(),
-            onPageChanged: (value) {
-//              print('OnPageChanged: $value');
-            },
-            itemCount: pages.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Center(
-                  child: Text(
-                pages[index],
-                style: TextStyle(color: pageTextColor),
-              ));
-            }),
+
+      // PageView (in the body)
+      body: PageView.builder(
+        itemCount: pageTitles.length,
+        controller: pageController,
+        reverse: false,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, page) {
+          return Center(
+            child: Text(
+              pageTitles[page],
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Theme.of(context).accentColor),
+            ),
+          );
+        },
       ),
+
+      // Bottom App Bar
       bottomNavigationBar: AnimatedBuilder(
         animation: pageController,
         builder: (BuildContext context, Widget child) {
           return BottomNavigationBar(
-            backgroundColor: bottomNavigationBarColor,
-            onTap: (page){
-              pageController.animateToPage(page, duration: Duration(milliseconds: 500), curve: Curves.easeInOutBack);
-            },
+            type: BottomNavigationBarType.fixed,
             currentIndex: pageController.page.round(),
-            items: <BottomNavigationBarItem>[
+            onTap: (int page) {
+              pageController.animateToPage(page,
+                  curve: Curves.easeInOutBack,
+                  duration: Duration(milliseconds: 500));
+            },
+            fixedColor: Theme.of(context).accentColor,
+            backgroundColor: Theme.of(context).bottomAppBarColor,
+            unselectedIconTheme:
+                IconThemeData(color: Theme.of(context).disabledColor),
+            items: [
               BottomNavigationBarItem(
-                  icon: Icon(Icons.home, color: iconsColor),
-                  title: Text(
-                    'Home',
-                    style: TextStyle(color: iconsTextColor),
-                  )),
+                  icon: Icon(Icons.home), title: Text(pageTitles[0])),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.star, color: iconsColor),
-                  title:
-                  Text('Favorites', style: TextStyle(color: iconsTextColor))),
+                  icon: Icon(
+                    Icons.rss_feed,
+                  ),
+                  title: Text(pageTitles[1])),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle, color: iconsColor),
-                  title: Text('Account', style: TextStyle(color: iconsTextColor)))
+                  icon: Icon(Icons.star), title: Text(pageTitles[2])),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle), title: Text(pageTitles[3])),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings_applications),
+                  title: Text(pageTitles[4])),
             ],
           );
         },
