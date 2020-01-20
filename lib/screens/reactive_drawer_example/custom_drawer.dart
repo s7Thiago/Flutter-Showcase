a1e43afff436967a9c1479751d81721a1477c86a
s7thiago <thyagosousasilva@gmail.com>
2020-01-20 10:04:04 -0300

    Example of drawer navigation with setState

--- lib/screens/reactive_drawer_example/custom_drawer.dart
+++ lib/screens/reactive_drawer_example/custom_drawer.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatelessWidget {
+  final Function(int) onPressed;
+
+  const CustomDrawer({Key key, this.onPressed}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -8,12 +12,21 @@ class CustomDrawer extends StatelessWidget {
         children: <Widget>[
           ListTile(
             title: Text('Home'),
+            onTap: () {
+              onPressed(0);
+            },
           ),
           ListTile(
             title: Text('Favorites'),
+            onTap: () {
+              onPressed(1);
+            },
           ),
           ListTile(
             title: Text('Account'),
+            onTap: () {
+              onPressed(2);
+            },
           ),
           ListTile(
             title: Text('Exit'),
